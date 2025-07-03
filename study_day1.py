ce6420724a6bc304cef486e4fd5682a20324f3e6
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-07-03 19:12:37 +0900

    add:　リストの作成、各種演算子の練習

--- study_day1.py
+++ study_day1.py
@@ -39,3 +39,62 @@ b = 1
 bool01 = (a < b)
 print(bool01)
 print(type(bool01))
+
+# リストの作成
+a = ["sato","suzuki","takahashi"]
+print(a)
+print(a[0])
+print(a[1])
+print(a[2])
+
+a[1] = "tanaka"
+print(a[1])
+
+# リストの内部にリストを作成
+a = [["sato","suzuki"],["takahashi","tanaka"]]
+print(a[0][0])
+print(a[0][1])
+print(a[1][0])
+print(a[1][1])
+
+# 演算子での計算
+x = 10
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)# %は剰余
+
+# 関係演算子
+print(x > y)
+print(x < y)
+
+z = 10
+print(x >= z)
+
+# 等価は == 、非等価は != で表現する
+print(x == z)
+print(x != z)
+
+# 論理演算
+x = 8
+y = 3
+
+print(x >= 5 and x <= 10)
+print(y >= 5 and y <= 10)
+print(x == 3 or y == 3)
+print(x == 1 or y == 1)
+
+# 代入演算子
+x = 8
+y = 12
+z = 20
+
+# xに10を加算し、その数値をxに代入する
+x +=10
+# zにyを加算し、その数値をzに代入する
+z += y
+
+print(x)
+print(z)
