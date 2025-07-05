5c3a5790990bcd299b901e1bb900dfb8794aa81c
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-07-05 23:19:17 +0900

    add:メソッドの追加

--- study_day3.py
+++ study_day3.py
@@ -7,9 +7,20 @@ class Person():
     
     def say_hello(self): # メソッドの定義
         print(f"こんにちは、私は{self.name}、{self.age}歳です")
+    
+    def is_adult(self): # 年齢で成人かどうかを判別するメソッド追加
+        if self.age >= 20:
+            print("私は大人です")
+        else:
+            print("私は未成年です")
 
-# クラスからインスタンス（オブジェクト）を作る
+# インスタンス作成
 person1 = Person("石井",24)
+person2 = Person("田中",18)
 
 # メソッドを呼び出す
 person1.say_hello()
+person1.is_adult()
+
+person2.say_hello()
+person2.is_adult()
