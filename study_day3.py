3991369f06d8623766a9ff243b779ee628135871
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-07-05 23:23:24 +0900

    add:コンストラクタの追加

--- study_day3.py
+++ study_day3.py
@@ -1,10 +1,11 @@
 # クラスの作成
 # Personクラスの定義
 class Person():
-    def __init__(self,name,age): # コンストラクタの定義
+    def __init__(self,name,age,height): # コンストラクタの定義
         self.name = name # 名前
         self.age = age # 年齢
-    
+        self.height = height # 身長
+
     def say_hello(self): # メソッドの定義
         print(f"こんにちは、私は{self.name}、{self.age}歳です")
     
@@ -14,13 +15,19 @@ class Person():
         else:
             print("私は未成年です")
 
+    def is_height(self):
+        if self.height >= 170:
+            print("高いです")
+
 # インスタンス作成
-person1 = Person("石井",24)
-person2 = Person("田中",18)
+person1 = Person("石井",24,169)
+person2 = Person("田中",18,180)
 
 # メソッドを呼び出す
 person1.say_hello()
 person1.is_adult()
+person1.is_height()
 
 person2.say_hello()
 person2.is_adult()
+person2.is_height()
